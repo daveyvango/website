b6d2a6b9cd6152a5983f7b31787a23320fef3756
Your Name <vangordd@gmail.com>
2019-01-17 23:13:22 +0000

    Adding new blog simple functionality

--- personalpage/blog/urls.py
+++ personalpage/blog/urls.py
@@ -6,5 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:blog_id>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
     path('author/<int:author_id>', views.author_detail, name='author_detail'),
 ]
--- personalpage/blog/views.py
+++ personalpage/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from django.utils import timezone
 
 from .models import BlogPost, Author
 
@@ -15,7 +19,23 @@ def author_detail(request, author_id):
     author = Author.objects.get(pk=author_id)
     return render(request, 'blog/author_detail.html', {'author': author })
 
-def create(request, blog_id):
-    blog_post = BlogPost.objects.get(pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog_post': blog_post})
+def new(request):
+    return render(request, 'blog/new.html')
 
+def create(request):
+    try:
+        handle     = request.POST['author_handle']
+        post_title = request.POST['post_title']
+        post_text  = request.POST['post_text']
+
+        author = Author.objects.filter(handle=handle)[0]
+        blog_post = BlogPost(text=post_text, author=author, title=post_title, post_date=timezone.now())
+        blog_post.save()
+    except:
+        # Redisplay the question voting form.
+        return HttpResponse("something broke.")
+    else:
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('blog:detail', args=(blog_post.id,)))
